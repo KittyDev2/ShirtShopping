f96fbb17944a6bac2d2a63bb1db000982149713e
KittyDev2 <erick.naacimentolb@gmail.com>
2024-05-21 13:41:08 -0300

    Adicionar arquivos de projeto.

---
+++ ShirtShooping/ShirtShooping.PoductAPI/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ShirtShooping.PoductAPI.Data.ValueObjects;
+using ShirtShooping.PoductAPI.Model;
+
+namespace ShirtShooping.PoductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps() 
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ ShirtShooping/ShirtShooping.PoductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShirtShooping.PoductAPI.Data.ValueObjects;
+using ShirtShooping.PoductAPI.Model;
+
+namespace ShirtShooping.PoductAPI.Repository;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<ProductVO>> FindAll();
+    Task<ProductVO> FindById(long id);
+    Task<ProductVO> Create(ProductVO vo);
+    Task<ProductVO> Update(ProductVO vo);
+    Task<bool> Delete(long id);
+
+}
