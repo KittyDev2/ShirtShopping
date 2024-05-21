0e729a918f03224ebf8486b0e3433b4e179cb255
KittyDev2 <erick.naacimentolb@gmail.com>
2024-05-21 14:53:14 -0300

    add CRUD and add new images, delete default classes c#.

--- ShirtShooping/ShirtShooping.PoductAPI/Model/Context/MySQLContext.cs
+++ ShirtShooping/ShirtShooping.PoductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,18 @@ namespace ShirtShooping.PoductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
 
         public DbSet<Product> Products { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Name",
+                Price = new decimal (69.9),
+                Description = "Camisa Palmeiras - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/768978_01.png?raw=true",
+                CategoryName = "Football T-Shirt"
+            });
+        }
     }
 }
--- ShirtShooping/ShirtShooping.PoductAPI/Repository/ProductRepository.cs
+++ ShirtShooping/ShirtShooping.PoductAPI/Repository/ProductRepository.cs
@@ -48,8 +48,9 @@ namespace ShirtShooping.PoductAPI.Repository
         {
             try
             {
-                Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
-                if (product != null) return false;
+                Product product = await _context.Products.Where(p => p.Id == id).
+                    FirstOrDefaultAsync();
+                if (product == null) return false;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
                 return true;
