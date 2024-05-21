aae5a6bdb44f29c11924396326dacd0b5091f7ef
KittyDev2 <erick.naacimentolb@gmail.com>
2024-05-21 15:00:23 -0300

    add migrations and add data in DB

--- ShirtShooping/ShirtShooping.PoductAPI/Model/Context/MySQLContext.cs
+++ ShirtShooping/ShirtShooping.PoductAPI/Model/Context/MySQLContext.cs
@@ -14,10 +14,50 @@ namespace ShirtShooping.PoductAPI.Model.Context
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 Id = 2,
-                Name = "Name",
-                Price = new decimal (69.9),
-                Description = "Camisa Palmeiras - XG",
-                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/768978_01.png?raw=true",
+                Name = "Camisa São Paulo",
+                Price = new decimal (120.9),
+                Description = "Camisa São Paulo Oficial - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/S%C3%A3o-Paulo-FC.jpg?raw=true",
+                CategoryName = "Football T-Shirt"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Camisa Corinthians",
+                Price = new decimal(120.9),
+                Description = "Camisa Corinthians Oficial - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/Corinthians-FC.jpg?raw=true",
+                CategoryName = "Football T-Shirt"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camisa Santos",
+                Price = new decimal(120.9),
+                Description = "Camisa Santos Oficial - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/Santos-FC.jpeg?raw=true",
+                CategoryName = "Football T-Shirt"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Camisa Flamengo",
+                Price = new decimal(120.9),
+                Description = "Camisa Flamengo Oficial - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/Flamengo-FC.jpg?raw=true",
+                CategoryName = "Football T-Shirt"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Camisa Cruzeiro",
+                Price = new decimal(120.9),
+                Description = "Camisa Cruzeiro Oficial - XG",
+                ImageUrl = "https://github.com/KittyDev2/ShirtShopping/blob/master/ShoppingImages/Cruzeiro-FC.jpg?raw=true",
                 CategoryName = "Football T-Shirt"
             });
         }
