4d1dafa5a585617c36e3007728549a5c0a4e923b
Kitty-Dev2 <erick.naacimentolb@gmail.com>
2024-05-22 01:00:25 -0300

    Consumindo o serviços web com a API.

---
+++ ShirtShooping/ShirtShooping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ShirtShooping.Web.Models;
+
+namespace ShirtShooping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
