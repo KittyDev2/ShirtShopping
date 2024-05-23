eee8ee9b1b87eb2ccb75de5f8d75a2e9b6dfee3d
KittyDev2 <erick.naacimentolb@gmail.com>
2024-05-23 10:58:30 -0300

    add new project IdentityServr with Duende.

---
+++ ShirtShooping/ShirtShooping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShirtShooping.IdentityServer.Model;
+
+public class ApplicationUser : IdentityUser
+{
+    private string FirstName { get; set; }
+    private string LastName { get; set; }
+}
---
+++ ShirtShooping/ShirtShooping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShirtShooping.IdentityServer.Model.Context;
+
+public class MySQLContext : IdentityDbContext<ApplicationUser>
+{
+    public MySQLContext(DbContextOptions<MySQLContext> options) 
+        : base(options)
+    {
+    }
+}
