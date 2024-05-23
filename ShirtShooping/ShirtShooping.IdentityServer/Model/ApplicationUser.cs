bd0fcbd3e6e11430ea8f0b953ed512e068b1d3c1
KittyDev2 <erick.naacimentolb@gmail.com>
2024-05-23 11:01:14 -0300

    Revert "add new project IdentityServr with Duende."
    This reverts commit eee8ee9b1b87eb2ccb75de5f8d75a2e9b6dfee3d.

--- ShirtShooping/ShirtShooping.IdentityServer/Model/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ShirtShooping.IdentityServer.Model;
-
-public class ApplicationUser : IdentityUser
-{
-    private string FirstName { get; set; }
-    private string LastName { get; set; }
-}
--- ShirtShooping/ShirtShooping.IdentityServer/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ShirtShooping.IdentityServer.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
